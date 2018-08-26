20ab1172899057020bc6c13350cfb14015f50ada
Ove Doll <ove.doll@aboutyou.de>
2018-08-26 12:32:19 +0200

    add Typescript to modules

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,13 +9,7 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-transformer-remark',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: `${__dirname}/src/images`,
-      },
-    },
+    'gatsby-plugin-typescript',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
